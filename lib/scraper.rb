fd4da0b17ca594ce9c552d4c68ad799480df521b
Terren Culberson <culbersonterren@yahoo.com>
2020-01-29 03:34:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+#html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+#doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-doc.css(".headline-26OIBN")
+#doc.css(".headline-26OIBN")
 
-puts doc.css(".headline-26OIBN")
+#puts doc.css(".headline-26OIBN")
+
