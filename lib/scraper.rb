04705444892fffa5d11952c145b3e3acca3ef89f
Terren Culberson <culbersonterren@yahoo.com>
2020-01-29 03:57:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,3 +9,7 @@ require 'open-uri'
 
 #puts doc.css(".headline-26OIBN")
 
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
